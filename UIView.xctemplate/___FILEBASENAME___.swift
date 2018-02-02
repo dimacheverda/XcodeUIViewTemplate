bc800bdd723097047526c526dc343867be415198
Dmytro Cheverda <cheverda@sjinnovation.com>
2018-02-02 20:36:17 +0200

    Initial commit

---
+++ UIView.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,15 @@
+//  ___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAME___: ___VARIABLE_subclassesFrom___ {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
